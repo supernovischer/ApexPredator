773bc27f500d42b4cf435d1265c7c45e5539202c
supernovischer <alletseej4@gmail.com>
2022-12-05 22:37:42 +0100

    added mongo db

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,5 @@ dotenv.config();
 export const TOKEN = process.env.TOKEN || '';
 export const APPLICTION_ID = process.env.APPLICTION_ID || '';
 export const GUILD_ID = process.env.GUILD_ID || '';
+export const MONGO_DATABASE_NAME = process.env.MONGO_DATABASE_NAME || '';
+export const MONGO_URI = process.env.MONGO_URI || '';
